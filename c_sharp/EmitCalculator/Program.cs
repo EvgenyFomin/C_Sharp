02fb1fe2bc9d484667ca855f956dd669813620aa
EvgenyFomin <sevenup96@mail.ru>
2017-10-19 20:54:24 +0300

    homeWorks

---
+++ c_sharp/EmitCalculator/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace EmitCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generator g = new Generator("(a + b) * c + d");
+            Func<int,int,int,int,int>
+                f0 = g.GenerateFunc<int>();
+            Func<double,double,double,double,double>
+                f1 = g.GenerateFunc<double>();
+            Console.WriteLine(f0(2, 3, 4, 5));
+            Console.WriteLine(f1(2.1, 3.2, 4.3, 5.4));
+        }
+    }
+
+    class Generator
+    {
+        private string s;
+        private int pos;
+        private char c;
+        private bool end;
+        private ILGenerator il;
+        private DynamicMethod mth;
+        private char[] lvSign = new char[] { '*', '+' };
+        private OpCode[] lvCode = new OpCode[] { OpCodes.Mul, OpCodes.Add };
+
+        public Generator(string expression)
+        {
+            s = expression;
+        }
+
+        public Func<T,T,T,T,T> GenerateFunc<T>()
+        {
+            GenerateMethod(typeof(T));
+            Delegate res = mth.CreateDelegate(typeof(Func<T,T,T,T,T>));
+            return (Func<T,T,T,T,T>)res;
+        }
+
+        private void GenerateMethod(Type t)
+        {
+            Type[] methodArgs = new Type[4];
+            for (int i = 0; i < 4; i++)
+                methodArgs[i] = t;
+            mth = new DynamicMethod("f", t, methodArgs,
+                typeof(Generator).Module );
+
+            il = mth.GetILGenerator();
+            pos = 0;
+            end = false;
+            Next();
+            GenerateExpression();
+            il.Emit(OpCodes.Ret);
+        }
+
+        private void Next()
+        {
+            while (pos < s.Length)
+            {
+                c = s[pos++];
+                if (c != ' ')
+                    break;
+            }
+            if (pos == s.Length)
+                end = true;
+        }
+
+        private void GenerateExpression(int lv = 1)
+        {
+            if (lv < 0)
+            {
+                if (c == '(')
+                {
+                    Next();
+                    GenerateExpression();
+                }
+                else /* if (c >= 'a' && c <= 'd') */
+                    il.Emit(OpCodes.Ldarg, c - 'a');
+                Next();
+                return;
+            }
+            GenerateExpression(lv - 1);
+            while (!end && c == lvSign[lv])
+            {
+                Next();
+                GenerateExpression(lv - 1);
+                il.Emit(lvCode[lv]);
+            }
+        }
+    }
+}
---
+++ c_sharp/PatchIL/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Utilities;
+using System.Diagnostics;
+using System.IO;
+
+namespace PatchIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string assemblyPath = args[0];
+            FileInfo assemblyFile = new FileInfo(assemblyPath);
+            string assemblyFileName = assemblyFile.Name;
+            Directory.SetCurrentDirectory(assemblyFile.Directory.FullName);
+            string ilName = "ilcode.il";
+            TargetDotNetFrameworkVersion sdkVersion = TargetDotNetFrameworkVersion.VersionLatest;
+            string ildasmPath = ToolLocationHelper.GetPathToDotNetFrameworkSdkFile("ildasm.exe", sdkVersion);
+
+            /* Где-то это работает, у меня нет */
+            string ilasmPath = ToolLocationHelper.GetPathToDotNetFrameworkSdkFile("ilasm.exe", sdkVersion);
+            /* В какой-то библиотеке используется такой "хак" */
+            if (string.IsNullOrEmpty(ilasmPath))
+                ilasmPath = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\ilasm.exe";
+
+            string ildasmArgs = string.Format("/out={0} {1}", ilName, assemblyFileName);
+            string ilasmArgs = string.Format("/out={0} {1}", assemblyFileName, ilName);
+
+            ProcessStartInfo ildasmStart = new ProcessStartInfo(ildasmPath, ildasmArgs);
+            ildasmStart.WindowStyle = ProcessWindowStyle.Hidden;
+            ProcessStartInfo ilasmStart = new ProcessStartInfo(ilasmPath, ilasmArgs);
+            ilasmStart.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process p;
+            
+            p = Process.Start(ildasmStart);
+            p.WaitForExit();
+
+            string ilCode = File.ReadAllText(ilName);
+            string ilModified = ilCode.Replace("Hello, World!", "Goodbye, World!");
+
+            File.WriteAllText(ilName, ilModified);
+
+            p = Process.Start(ilasmStart);
+            p.WaitForExit();
+        }
+    }
+}
